562ab2daca30089c192862ce7aa5216b83cd06c9
Hoang - UIT <nhoang200260@gmail.com>
2024-04-26 15:08:44 +0700

    create routes and controller

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+
+const app = express()
+const port = 3000
+
+const route = require('./routes')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// HTTP logger
+// app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', engine({
+    extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+
+//route init
+route(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
